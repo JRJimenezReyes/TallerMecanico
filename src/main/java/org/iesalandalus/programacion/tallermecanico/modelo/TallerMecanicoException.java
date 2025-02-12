a135ff69e8a09fb5935aef54ea497ecd2b5d5569
asilper <asliper86@gmail.com>
2025-02-12 12:27:27 +0100

    Excepcion terminada

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/TallerMecanicoException.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/TallerMecanicoException.java
@@ -1,6 +1,6 @@
 package org.iesalandalus.programacion.tallermecanico.modelo;
 
-public class TallerMecanicoException extends RuntimeException {
+public class TallerMecanicoException extends Exception {
     public TallerMecanicoException(String message) {
         super(message);
     }
