c5d5553eed40f4ccfb1752d21506769a86433028
Adrian Siles <156607183+Asilper86@users.noreply.github.com>
2025-02-17 13:49:36 +0000

    Subida archivos.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/TallerMecanicoException.java
+++
@@ -1,7 +0,0 @@
-package org.iesalandalus.programacion.tallermecanico.modelo;
-
-public class TallerMecanicoException extends Exception {
-    public TallerMecanicoException(String message) {
-        super(message);
-    }
-}
